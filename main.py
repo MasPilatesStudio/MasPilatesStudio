395a6e88292062372c27437c52023b638ac356c7
lluminar <60161182+lluminar@users.noreply.github.com>
2022-03-13 21:25:44 +0100

    segundo commit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
     print('Hello, World!')
-    return 'Hello'
+    return jsonify({'message':'Hello'})
+
+if __name__=="__main__":
+    app.run(debug==True)
